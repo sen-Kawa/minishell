aaa236333f88438490a37b7e563d625450804674
KSura <kevin.sura@outlook.de>
2022-08-17 12:32:08 +0200

    header

--- Module0_Init/minishell.h
+++ Module0_Init/minishell.h
@@ -1,13 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksura@student.42wolfsburg.de <ksura@studen +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/17 12:30:14 by ksura@student.42  #+#    #+#             */
-/*   Updated: 2022/08/17 12:30:47 by ksura@student.42 ###   ########.fr       */
+/*   Created: 2022/08/17 12:20:23 by ksura@student.42  #+#    #+#             */
+/*   Updated: 2022/08/17 12:30:00 by ksura@student.42 ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+// #include <readline/readline.h>
+// #include <readline/history.h>
+
+#endif
