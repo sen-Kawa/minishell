7f63f6682946f837b6c5499bd5955ca8f384fd3d
olin_tp_ar <myemail@archi.com>
2022-09-09 14:35:00 +0200

    starting with signals

--- main.c
+++ main.c
@@ -2,14 +2,27 @@
 
 char *command;
 
+void	handler(int num)
+{
+	(void)num;
+	write(0, "\n", 1);
+}
+
 int	main(void)
 {
+	struct	sigaction	sa;
+	pid_t	pid;
+
+	pid = getpid();
+	ft_printf("pid is %d\n", pid);
 	while (1)
 	{
+		sa.sa_handler = &handler;
+		sigaction(SIGINT, &sa, NULL);
+		write(1, "1", 1);
 		command = readline("cli prompt >> ");
 		if (ft_strlen(command) > 0)
 			add_history(command);
 	}
-	//	write(1, "1", 1);
 	return 0;
 }
