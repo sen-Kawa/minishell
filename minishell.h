4676400b1c7313506ec4647e2a1149ebe6364092
olin_tp_ar <myemail@archi.com>
2022-09-09 09:30:25 +0200

    compiles with the readline library

--- minishell.h
+++ minishell.h
@@ -6,17 +6,17 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 08:59:06 by ksura             #+#    #+#             */
-/*   Updated: 2022/09/09 09:26:23 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/09/09 09:29:02 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/libft.h"
+# include "libft/libft.h"
+# include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <stdio.h>
 # include <unistd.h>
 
 #endif
