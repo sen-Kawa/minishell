e19819db1bf23573f1e96e5f31cfe262c167d67b
olin_tp_ar <myemail@archi.com>
2022-09-09 17:13:57 +0200

    controls handled

--- main.c
+++ main.c
@@ -2,41 +2,20 @@
 
 char *command;
 
-// void	handler(int num)
-// {
-// 	(void)num;
-// 	// write(0, "\n", 1);
-// 	// ft_putstr_fd("\b\b\n", 1);
-// 	// rl_replace_line("", 0);
-// 	// rl_on_new_line();
-// 	// rl_redisplay();
-// 	// exit(1);
-// }
-
 void	handler_quit(int sig)
 {
-	// write(0, "\n", 1);
-	if (sig ==  SIGINT)
+	if (sig == SIGINT)
 	{
-		// exit (1);
-		ft_putstr_fd("\b\b\n", 1);
 		rl_replace_line("", 0);
 		rl_on_new_line();
+		ft_putstr_fd("\b\b\n", 1);
 		rl_redisplay();
 	}
-	if (sig ==  SIGQUIT)
-	{
-		exit(1);
-		write(1, "1", 1);
-		// signal(SIGQUIT, SIG_DFL);
-	}
-	
-	// exit(1);
 }
 
 int	main(void)
 {
-	struct	sigaction	sa;
+	struct	sigaction sa;
 	pid_t	pid;
 
 	pid = getpid();
@@ -45,10 +24,14 @@ int	main(void)
 	{
 		sa.sa_handler = &handler_quit;
 		sigaction(SIGINT, &sa, NULL);
-		sigaction(SIGQUIT, &sa, NULL);
-		// write(1, "1", 1);
+		signal(SIGQUIT, SIG_IGN);
 		command = readline("cli prompt >> ");
-		if (command && *command)
+		if (command == NULL)
+		{
+			write(1, "exit\n", 5);
+			break ;
+		}			
+		else if (command && *command)
 			add_history(command);
 	}
 	return 0;
