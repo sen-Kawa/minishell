d7c966cdff0b224af75662d86441d1dfd0caf931
ksura12 <kevin.sura@outlook.de>
2022-09-09 16:22:58 +0200

    SIGINT

--- main.c
+++ main.c
@@ -2,14 +2,35 @@
 
 char *command;
 
-void	handler(int num)
+// void	handler(int num)
+// {
+// 	(void)num;
+// 	// write(0, "\n", 1);
+// 	// ft_putstr_fd("\b\b\n", 1);
+// 	// rl_replace_line("", 0);
+// 	// rl_on_new_line();
+// 	// rl_redisplay();
+// 	// exit(1);
+// }
+
+void	handler_quit(int sig)
 {
-	(void)num;
 	// write(0, "\n", 1);
-	ft_putstr_fd("\b\b\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
+	if (sig ==  SIGINT)
+	{
+		// exit (1);
+		ft_putstr_fd("\b\b\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	if (sig ==  SIGQUIT)
+	{
+		exit(1);
+		write(1, "1", 1);
+		// signal(SIGQUIT, SIG_DFL);
+	}
+	
 	// exit(1);
 }
 
@@ -22,11 +43,12 @@ int	main(void)
 	ft_printf("pid is %d\n", pid);
 	while (1)
 	{
-		sa.sa_handler = &handler;
+		sa.sa_handler = &handler_quit;
 		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa, NULL);
 		// write(1, "1", 1);
 		command = readline("cli prompt >> ");
-		if (ft_strlen(command) > 0)
+		if (command && *command)
 			add_history(command);
 	}
 	return 0;
