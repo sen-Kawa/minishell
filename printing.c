35270bbd2f0ba6a1e6e43dd9dbe444ee6bf77e49
ksura12 <kevin.sura@outlook.de>
2022-09-12 18:25:15 +0200

    double qoutes somewhere, pairwise, without inner parsing

--- printing.c
+++ printing.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 11:31:23 by ksura             #+#    #+#             */
-/*   Updated: 2022/09/12 11:45:44 by ksura            ###   ########.fr       */
+/*   Updated: 2022/09/12 18:10:17 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void    printing_list_a(t_ms_list *tokens)
 {
         while (tokens)                                                          
         {
-                ft_printf("Node: %s\n", tokens->token);
+                ft_printf("Token: %s    Type: %s\n", tokens->token, tokens->type);
                 tokens = tokens->next;
         }   
         ft_printf("___\n");
--- tokeniser.c
+++ tokeniser.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 09:29:34 by ksura             #+#    #+#             */
-/*   Updated: 2022/09/12 15:30:00 by ksura            ###   ########.fr       */
+/*   Updated: 2022/09/12 18:23:57 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,33 +25,52 @@ void	tokenice(char *command, t_ms_list *tokens)
 	start = 0;
 	length = ft_strlen(command);
 	printf("The Length is:%i\n", length);
-	while (command[i] == ' ')
-		i++;
-	start = start + i;
-	i = 0;
+	while (command[start] == ' ')
+		start++;
 	while(command[start + i] && *command)
 	{
+	
+		
 		if (start + i <= length)
 		{
 			if (command[start + i] == ' ')
 			{
-				
-				part = ft_substr(command, start, i);
-				newbe = ft_tokennew(part, NULL);
-				// free(part);
-				ft_tokenadd_back(&tokens, newbe);
+				write(1, "inside 1\n", 10);
+				if (i > 0)
+				{
+					part = ft_substr(command, start, i);
+					newbe = ft_tokennew(part, "space");
+					ft_tokenadd_back(&tokens, newbe);
+				}
 				while (command[start + i] == ' ')
 					i++;
 				start = start + i;
 				i = -1;
 			}
 		}
+		
 		i++;
+		if (command[start + i] == '"')
+		{
+			write(1, "Quote\n", 7);
+			i++;
+			while (command[start + i] != '"')
+				i++;
+			
+			part = ft_substr(command, start + 1, i - 1);
+			newbe = ft_tokennew(part, "double quotes");
+			ft_tokenadd_back(&tokens, newbe);
+			i++;
+			start = start + i;
+			i = 0;
+		}
+		
 	}
 	if (i > 0)
 	{
+		write(1, "inside 2\n", 10);
 		part = ft_substr(command, start, i);
-		newbe = ft_tokennew(part, NULL);
+		newbe = ft_tokennew(part, "space_before");
 		// free(part);
 		ft_tokenadd_back(&tokens, newbe);
 	}
