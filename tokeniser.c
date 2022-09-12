8a70888974a2d86f2b188dd287ed7a06d037af58
olin_tp_ar <myemail@archi.com>
2022-09-12 14:38:31 +0200

    lexical analysis for spaces workingish, substracted strings into linked list

--- tokeniser.c
+++ tokeniser.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 09:29:34 by ksura             #+#    #+#             */
-/*   Updated: 2022/09/12 12:57:04 by ksura            ###   ########.fr       */
+/*   Updated: 2022/09/12 14:37:00 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,35 +25,29 @@ void	tokenice(char *command, t_ms_list *tokens)
 	start = 0;
 	length = ft_strlen(command);
 	printf("The Length is:%i", length);
+	while (command[i] == ' ')
+		i++;
+	start = start + i;
 	while(command[start + i] && *command)
 	{
 		if (start + i <= length)
 		{
 			if (command[start + i] == ' ')
 			{
-				while (command[start + i] == ' ' && command[start + i - 1] == ' ')
+				while (command[start + i] == ' ')
 					i++;
-				// if (command[start + i] == ' ' && command[start + i - 1] != ' ')
 				part = ft_substr(command, start, i);
 				newbe = ft_tokennew(part, NULL);
 				// free(part);
 				ft_tokenadd_back(&tokens, newbe);
-				start = start + i + 1;
+				start = start + i;
 				i = -1;
-				// printf("Yes");
-				//check if there is a list
-				// if yes lst_addback / no - new list
 			}
 		}
-		
-		// if (command[length] != ' ')
-		// {
-		// 	part = ft_substr(command, start, i);
-		// 	newbe = ft_tokennew(part, NULL);
-		// 	free(part);
-		// 	ft_tokenadd_back(&tokens, newbe);
-		// }
-		// printf("Yes");
 		i++;
 	}
+	part = ft_substr(command, start, i);
+	newbe = ft_tokennew(part, NULL);
+	// free(part);
+	ft_tokenadd_back(&tokens, newbe);
 }
