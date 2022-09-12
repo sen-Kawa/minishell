618d9e69ce1502b0b24abc561f6f30cf1a30859c
olin_tp_ar <myemail@archi.com>
2022-09-12 14:47:15 +0200

    resetting i to zero before the main while loop in tokenicee

--- tokeniser.c
+++ tokeniser.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 09:29:34 by ksura             #+#    #+#             */
-/*   Updated: 2022/09/12 14:37:00 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/09/12 14:46:01 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,23 @@ void	tokenice(char *command, t_ms_list *tokens)
 	i = 0;
 	start = 0;
 	length = ft_strlen(command);
-	printf("The Length is:%i", length);
+	printf("The Length is:%i\n", length);
 	while (command[i] == ' ')
 		i++;
 	start = start + i;
+	i = 0;
 	while(command[start + i] && *command)
 	{
 		if (start + i <= length)
 		{
 			if (command[start + i] == ' ')
 			{
-				while (command[start + i] == ' ')
-					i++;
 				part = ft_substr(command, start, i);
 				newbe = ft_tokennew(part, NULL);
 				// free(part);
 				ft_tokenadd_back(&tokens, newbe);
+				while (command[start + i] == ' ')
+					i++;
 				start = start + i;
 				i = -1;
 			}
