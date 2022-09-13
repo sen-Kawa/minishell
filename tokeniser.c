a674b06fa44cb8300463022ec5830838d0bee94b
ksura12 <kevin.sura@outlook.de>
2022-09-13 16:14:10 +0200

    building seperate tokens for cmd words with quotet sections in it

--- tokeniser.c
+++ tokeniser.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 09:29:34 by ksura             #+#    #+#             */
-/*   Updated: 2022/09/13 13:01:12 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/09/13 16:11:30 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,31 +35,31 @@ t_lex_struct	double_quotes(char *command, t_lex_struct lex, t_ms_list *tokens)
 
 	dlr = 0;
 	if (command[lex.start + lex.i] == '"')
+	{
+		lex.i++;
+		while (command[lex.start + lex.i] && command[lex.start + lex.i] != '"')
 		{
 			lex.i++;
-			while (command[lex.start + lex.i] && command[lex.start + lex.i] != '"')
-			{
-				lex.i++;
-				if (command[lex.start + lex.i] == '$')
-					dlr = 1;
-			}
-				
-			if(command[lex.start + lex.i] != '"')
-			{
-				lex.error = 1;
-				return (lex);
-			}
-			part = ft_substr(command, lex.start + 1, lex.i - 1);
-			if (dlr == 0)
-				newbe = ft_tokennew(part, "double quotes", tokens->section);
-			else
-				newbe = ft_tokennew(part, "$double$quote$", tokens->section);
-			ft_tokenadd_back(&tokens, newbe);
-			lex.i++;
-			lex.start = lex.start + lex.i;
-			lex.i = 0;
+			if (command[lex.start + lex.i] == '$')
+				dlr = 1;
 		}
-		return (lex);
+			
+		if(command[lex.start + lex.i] != '"')
+		{
+			lex.error = 1;
+			return (lex);
+		}
+		part = ft_substr(command, lex.start + 1, lex.i - 1);
+		if (dlr == 0)
+			newbe = ft_tokennew(part, "double quotes", tokens->section);
+		else
+			newbe = ft_tokennew(part, "$double$quote$", tokens->section);
+		ft_tokenadd_back(&tokens, newbe);
+		lex.i++;
+		lex.start = lex.start + lex.i;
+		lex.i = -1;
+	}
+	return (lex);
 }
 
 /*
@@ -97,7 +97,7 @@ t_lex_struct	single_quotes(char *command, t_lex_struct lex, t_ms_list *tokens)
 			ft_tokenadd_back(&tokens, newbe);
 			lex.i++;
 			lex.start = lex.start + lex.i;
-			lex.i = 0;
+			lex.i = -1;
 		}
 		return (lex);
 }
@@ -168,17 +168,32 @@ t_lex_struct	tokenice(char *command, t_ms_list *tokens)
 						newbe = ft_tokennew(part, "space", tokens->section);
 						ft_tokenadd_back(&tokens, newbe);
 					}
-					// free(part);
 				}
 				while (command[lex.start + lex.i] == ' ')
 					lex.i++;
 				lex.start = lex.start + lex.i;
 				lex.i = -1;
 			}
+			if (command[lex.start +lex.i] == '"' || command[lex.start +lex.i] == 39)
+			{
+				if (lex.i > 0)
+				{
+					if (!pipe_check(command, lex, tokens))
+					{
+						part = ft_substr(command,lex. start, lex.i);
+						newbe = ft_tokennew(part, "beforequotes", tokens->section);
+						ft_tokenadd_back(&tokens, newbe);
+					}
+				}
+				lex.start = lex.start + lex.i;
+				lex.i = 0;
+			}
+			
 		}
-		lex.i++;
+		
 		lex = double_quotes(command, lex, tokens);
 		lex = single_quotes(command, lex, tokens);
+		
 		if (lex.error == 1)
 		{
 			write(1, "bash: syntax error, quotes missing\n", 36);
@@ -186,6 +201,7 @@ t_lex_struct	tokenice(char *command, t_ms_list *tokens)
 		}
 		if (lex.error == 2)
 			write(1, "bash: syntax error, space missing\n", 35);
+		lex.i++;
 	}
 	if (lex.i > 0)
 	{
