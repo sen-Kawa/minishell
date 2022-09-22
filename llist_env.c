bf871b50d452ada83a679f9f79291ec1e411cdbe
olin_tp_ar <myemail@archi.com>
2022-09-23 01:15:14 +0200

    divided file from builtins when creating list from env

---
+++ llist_env.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   llist_env.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 14:47:18 by ksura             #+#    #+#             */
+/*   Updated: 2022/09/23 00:32:49 by kaheinz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+t_env	*ft_envvnew(char *content)
+{
+	t_env	*new_list;
+
+	new_list = malloc(sizeof (t_env));
+	if (!new_list)
+		return (NULL);
+	new_list->content = content;
+	new_list->next = NULL;
+	return (new_list);
+}
+
+t_env	*ft_envvlast(t_env *envv)
+{
+	t_env	*last;
+
+	if (!envv)
+		return (NULL);
+	last = envv;
+	while (last->next != NULL)
+		last = last->next;
+	return (last);
+}
+
+void	ft_envvadd_back(t_env **env, t_env *new)
+{
+	if (!*env)
+		*env = new;
+	else
+	{
+		ft_envvlast(*env)->next = new;
+	}
+}
+
+void	print_env(t_ms	*ms)
+{
+	while (ms->env_list)
+	{
+		printf("%s\n", ms->env_list->content);
+		ms->env_list = ms->env_list->next;
+	}
+}
