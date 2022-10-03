6bffedde41aff2552027555a6ce1973dc5d93d24
ksura12 <kevin.sura@outlook.de>
2022-10-03 19:39:39 +0200

    heredoc without making nodes or returning, not leakfree

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,11 +6,13 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:26:19 by ksura             #+#    #+#             */
-/*   Updated: 2022/09/29 13:30:40 by ksura            ###   ########.fr       */
+/*   Updated: 2022/10/03 19:22:54 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
+
+void	heredoc(t_ms *ms);
 
 void	execute(t_ms *ms)
 {
@@ -24,6 +26,7 @@ void	execute(t_ms *ms)
 	b_unset(ms);
 	b_echo(ms);
 	b_cd(ms);
+	heredoc(ms);
 	while(tmp)
 	{
 		len = ft_strlen(tmp->token);
@@ -35,4 +38,54 @@ void	execute(t_ms *ms)
 		tmp = tmp->next;
 	}
 	return ;
+}
+
+void	heredoc(t_ms *ms)
+{
+	t_ms_list	*tmp;
+	char		*delim;
+	char		*hereline;
+	char		*herecom;
+	//char		*tmp_here;
+
+	tmp = ms->tokenlist;
+	if(!ft_strncmp(tmp->token, "<<\0", 3))
+	{
+		if(tmp->next)
+			delim = tmp->next->token;
+		else
+		{
+			ft_printf("cli: syntax error near unexpected token `newline'");
+			ms->lex->error = 4;
+			return ;
+		}
+		hereline = "";
+		herecom = NULL;
+		while (ft_strncmp(hereline, delim, sizeof(delim)))
+		{
+			
+			hereline = readline("> ");
+		
+			if (!ft_strncmp(hereline, delim, sizeof(delim)))
+			{
+				free (hereline);
+				break;
+			}
+			if (herecom)
+			{
+				// tmp_here = herecom;
+				herecom = ft_strjoin(herecom, hereline);
+				herecom = ft_strjoin(herecom, "\n");
+			}
+			else
+				herecom = ft_strjoin(hereline, "\n");
+			
+			free (hereline);
+		}
+		if (herecom)
+			{
+				ft_printf("%s", herecom);
+				//free (tmp_here);
+			}
+	}
 }
