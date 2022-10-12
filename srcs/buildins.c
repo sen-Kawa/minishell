d608d8c241cc1f570f4f0ba2cd68483742287bf3
olin_tp_ar <myemail@archi.com>
2022-10-12 14:18:21 +0200

    different file for export buildin

--- srcs/buildins.c
+++ srcs/buildins.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 14:47:18 by ksura             #+#    #+#             */
-/*   Updated: 2022/10/12 14:10:34 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/10/12 14:15:24 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,70 +72,6 @@ int	b_env(char *token, t_ms *ms)
 		return (1);
 	}
 	return (0);
-}
-
-int	b_export(t_ms	*ms)
-{
-	int			result;
-	int			flag;
-	char		*copy;
-	char		**splitted;
-	t_env		*new;
-	t_env		*tmp_env;
-	t_ms_list	*tmp;
-
-	flag = 0;
-	tmp = ms->tokenlist;
-	if (!tmp)
-		return (0);
-	result = ft_strncmp(tmp->token, "export\0", 7);
-	if (result == 0 && tmp->next == NULL)
-	{
-		make_array(ms, ms->env_lst_size);
-		ms->exit_status = 0;
-		return (1);
-	}
-	if (result == 0 && (!ft_strchr(tmp->next->token, '=') || ft_strlen(tmp->next->token) == 1 || tmp->next->token[0] == '='))
-	{
-		ft_printf("ksh: export: `%s': not a valid identifier\n", tmp->next->token);
-		ms->exit_status = 1;
-		return (1);
-	}
-	while (tmp)
-	{
-		if (result == 0 && ft_strchr(tmp->token, '='))
-		{
-			copy = ft_strdup(tmp->token);
-			splitted = ft_split(copy, '=');
-			tmp_env = ms->env_list;
-			while (tmp_env && tmp_env->next && ft_strncmp(tmp->token, tmp_env->content, ft_strlen(splitted[0])))
-				tmp_env = tmp_env->next;
-			if (!ft_strncmp(tmp->token, tmp_env->content, ft_strlen(splitted[0])))
-			{
-				tmp_env->content = tmp->token;
-				flag++;
-			}
-			else
-			{
-				new = ft_envvnew(tmp->token);
-				ft_envvadd_back(&ms->env_list, new);
-				ms->env_lst_size++;
-				flag++;
-			}
-			free(splitted);
-		}
-		if (tmp->next)
-			tmp = tmp->next;
-		else
-			break ;
-	}
-	if (flag > 0)
-	{
-		ms->exit_status = 0;
-		return (1);
-	}
-	else
-		return (0);
 }
 
 int	b_unset(t_ms	*ms)
