68003784336cd9104511b3dc4175771ad4059d1b
ksura12 <kevin.sura@outlook.de>
2022-10-12 18:15:37 +0200

    splitting in redirecting.c the redir2 into thre functions

--- srcs/redirecting.c
+++ srcs/redirecting.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 08:54:08 by ksura             #+#    #+#             */
-/*   Updated: 2022/10/12 16:55:11 by ksura            ###   ########.fr       */
+/*   Updated: 2022/10/12 18:14:36 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,86 @@
 
 /*
 DESCRIPTION
+PartIV of redirecting().
+Alters types of tokens for redirection purposes.
+
+RETURN
+-
+
+PARAMETERS
+tmp: linked list for tokens
+
+EXTERNAL FUNCTIONS
+-
+*/
+int	redir4(t_ms_list *tmp, t_ms *ms, int outfd)
+{
+	if (!tmp->token[2])
+	{
+		if (tmp->next)
+		{
+			tmp->next->type = "delete";
+			if ((access (tmp->next->token, F_OK) == 0)
+				&& (access (tmp->next->token, W_OK) != 0))
+			{
+				ft_printf("ksh: %s: Permission denied\n", \
+				tmp->next->token);
+				ms->exit_status = 1;
+				ms->lex->error = 1;
+				return (1);
+			}
+		}
+		ms->pipes_struct->fd_file[outfd] = open(tmp->next->token, \
+		O_RDWR | O_APPEND | O_CREAT);
+	}
+	return (0);
+}
+
+/*
+DESCRIPTION
+PartIII of redirecting().
+Alters types of tokens for redirection purposes.
+
+RETURN
+-
+
+PARAMETERS
+tmp: linked list for tokens
+
+EXTERNAL FUNCTIONS
+-
+*/
+void	redir3(t_ms_list *tmp, t_ms *ms, int outfd)
+{
+	if (tmp->token[0] && tmp->token[1])
+	{
+		if (tmp->token[0] == '>' && tmp->token[1] == '>')
+		{
+			tmp->type = "delete";
+			if (redir4(tmp, ms, outfd) == 1)
+				return ;
+			else if (tmp->token[2] != '\0')
+			{
+				tmp->type = "delete";
+				if ((access (tmp->token + 2, F_OK) == 0)
+					&& (access (tmp->token + 2, W_OK) != 0))
+				{
+					ft_printf("ksh: %s: Permission denied\n", tmp->token + 2);
+					ms->exit_status = 1;
+					ms->lex->error = 1;
+					return ;
+				}
+				ms->pipes_struct->fd_file[outfd] = open(tmp->token + 2, \
+				O_RDWR | O_APPEND | O_CREAT);
+			}
+		}
+	}
+}
+
+/*
+DESCRIPTION
 PartII of redirecting().
 Alters types of tokens for redirection purposes.
-In case of << it gives types to the symbol and
-the delimiter but doesn't split them if they are together
 
 RETURN
 -
@@ -47,45 +123,7 @@ void	redir2(t_ms_list *tmp, t_ms	*ms, int outfd)
 			}
 		}
 	}
-	if (tmp->token[0] && tmp->token[1])
-	{
-		if (tmp->token[0] == '>' && tmp->token[1] == '>')
-		{
-			tmp->type = "delete";
-			if (!tmp->token[2])
-			{
-				if (tmp->next)
-				{
-					tmp->next->type = "delete";
-					if ((access (tmp->next->token, F_OK) == 0)
-						&& (access (tmp->next->token, W_OK) != 0))
-					{
-						ft_printf("ksh: %s: Permission denied\n", \
-						tmp->next->token);
-						ms->exit_status = 1;
-						ms->lex->error = 1;
-						return ;
-					}
-				}
-				ms->pipes_struct->fd_file[outfd] = open(tmp->next->token, \
-				O_RDWR | O_APPEND | O_CREAT);
-			}
-			else if (tmp->token[2] != '\0')
-			{
-				tmp->type = "delete";
-				if ((access (tmp->token + 2, F_OK) == 0)
-					&& (access (tmp->token + 2, W_OK) != 0))
-				{
-					ft_printf("ksh: %s: Permission denied\n", tmp->token + 2);
-					ms->exit_status = 1;
-					ms->lex->error = 1;
-					return ;
-				}
-				ms->pipes_struct->fd_file[outfd] = open(tmp->token + 2, \
-				O_RDWR | O_APPEND | O_CREAT);
-			}
-		}
-	}
+	redir3(tmp, ms, outfd);
 }
 
 /*
