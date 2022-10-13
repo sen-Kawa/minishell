529d4e9a29068c9955de6d7c43a75e8fb3e4b947
olin_tp_ar <myemail@archi.com>
2022-10-13 16:05:38 +0200

    trying to divide heredoc, not working fully yet

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,13 +6,15 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:26:19 by ksura             #+#    #+#             */
-/*   Updated: 2022/10/13 14:42:46 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/10/13 15:50:55 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 #include <sys/types.h>
 #include <sys/wait.h>
+
+char	*here_command(char *hereline);
 
 void	heredoc(t_ms *ms, char	*delim)
 {
@@ -26,7 +28,8 @@ void	heredoc(t_ms *ms, char	*delim)
 		hereline = readline("> ");
 		if (hereline == NULL)
 		{
-			ft_printf("ksh: warning: here-document delimited by end-of-file(wanted `%s')\n", delim);
+			ft_printf("ksh: warning: \
+				here-document delimited by end-of-file(wanted `%s')\n", delim);
 			ms->exit_status = 1;
 			break ;
 		}
@@ -34,23 +37,34 @@ void	heredoc(t_ms *ms, char	*delim)
 		{
 			if (hereline)
 				free (hereline);
-			ms->pipes_struct->fd_file[0] = open(".tmp_heredoc", O_RDONLY | O_CREAT | O_TRUNC, 0777);
-			break;
+			ms->pipes_struct->fd_file[0] = open(".tmp_heredoc",
+					O_RDONLY | O_CREAT | O_TRUNC, 0777);
+			break ;
 		}
-		if (herecom)
-		{
-			herecom = ft_strjoin(herecom, hereline);
-			herecom = ft_strjoin(herecom, "\n");
-		}
-		else
-			herecom = ft_strjoin(hereline, "\n");
+		herecom = here_command(hereline);
 	}
 	if (herecom)
 	{
-		ms->pipes_struct->fd_file[0] = open(".tmp_heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		ms->pipes_struct->fd_file[0] = open(".tmp_heredoc",
+				O_WRONLY | O_CREAT | O_TRUNC, 0777);
 		if (ms->pipes_struct->fd_file[0] != -1)
 			ft_putstr_fd(herecom, ms->pipes_struct->fd_file[0]);
 		close(ms->pipes_struct->fd_file[0]);
 		ms->pipes_struct->fd_file[0] = open(".tmp_heredoc", O_RDONLY);
 	}
+}
+
+char	*here_command(char *hereline)
+{
+	char		*herecom;
+
+	herecom = NULL;
+	if (herecom)
+	{
+		herecom = ft_strjoin(herecom, hereline);
+		herecom = ft_strjoin(herecom, "\n");
+	}
+	else
+		herecom = ft_strjoin(hereline, "\n");
+	return (herecom);
 }
