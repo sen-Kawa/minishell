9cc957d32bf2bb2960c05a2b78849810199615c2
olin_tp_ar <myemail@archi.com>
2022-10-13 14:31:31 +0200

    removed heredoc from execution

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:26:19 by ksura             #+#    #+#             */
-/*   Updated: 2022/10/13 14:24:19 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/10/13 14:30:42 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,54 +36,6 @@ int	builtins(t_ms *ms)
 		tmp = tmp->next;
 	}
 	return (sum);
-}
-
-void	signal_heredoc(int sig)
-{
-		if (sig == SIGINT)
-				printf("signal in heredoc %i", sig);
-}
-
-void	heredoc(t_ms *ms, char	*delim)
-{
-	char		*hereline;
-	char		*herecom;
-
-	hereline = "";
-	herecom = NULL;
-	while (ft_strncmp(hereline, delim, sizeof(delim)))
-	{
-	//	signal(SIGINT, signal_heredoc);
-		hereline = readline("> ");
-		if (hereline == NULL)
-		{
-			ft_printf("ksh: warning: here-document delimited by end-of-file (wanted `%s')\n", delim);
-			ms->exit_status = 1;
-			break ;
-		}
-		if (!ft_strncmp(hereline, delim, sizeof(delim)))
-		{
-			if (hereline)
-				free (hereline);
-			ms->pipes_struct->fd_file[0] = open(".tmp_heredoc", O_RDONLY | O_CREAT | O_TRUNC, 0777);
-			break;
-		}
-		if (herecom)
-		{
-			herecom = ft_strjoin(herecom, hereline);
-			herecom = ft_strjoin(herecom, "\n");
-		}
-		else
-			herecom = ft_strjoin(hereline, "\n");
-	}
-	if (herecom)
-	{
-		ms->pipes_struct->fd_file[0] = open(".tmp_heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0777);
-		if (ms->pipes_struct->fd_file[0] != -1)
-			ft_putstr_fd(herecom, ms->pipes_struct->fd_file[0]);
-		close(ms->pipes_struct->fd_file[0]);
-		ms->pipes_struct->fd_file[0] = open(".tmp_heredoc", O_RDONLY);
-	}
 }
 
 void    child_signal(int sig)
