a166da784f0b11ff96c2c2dd042adb6878d3522d
olin_tp_ar <myemail@archi.com>
2022-10-18 13:25:37 +0200

    working and norminetted heredoc

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:26:19 by ksura             #+#    #+#             */
-/*   Updated: 2022/10/18 12:34:36 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/10/18 13:24:52 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void	here_command(char *hereline, char *herecom);
+void	here_command(char *hereline, char **herecom);
+void	tmp_heredoc(t_ms *ms, char *herecom);
 
 void	heredoc(t_ms *ms, char	*delim)
 {
@@ -41,9 +42,24 @@ void	heredoc(t_ms *ms, char	*delim)
 					O_RDONLY | O_CREAT | O_TRUNC, 0777);
 			break ;
 		}
-		here_command(hereline, herecom);
-		printf("herecom is: %s\n", herecom);
+		here_command(hereline, &herecom);
 	}
+	tmp_heredoc(ms, herecom);
+}
+
+void	here_command(char *hereline, char **herecom)
+{
+	if (herecom)
+	{
+		*herecom = ft_strjoin(*herecom, hereline);
+		*herecom = ft_strjoin(*herecom, "\n");
+	}
+	else
+		*herecom = ft_strjoin(hereline, "\n");
+}
+
+void	tmp_heredoc(t_ms *ms, char *herecom)
+{
 	if (herecom)
 	{
 		ms->pipes_struct->fd_file[0] = open(".tmp_heredoc",
@@ -53,25 +69,4 @@ void	heredoc(t_ms *ms, char	*delim)
 		close(ms->pipes_struct->fd_file[0]);
 		ms->pipes_struct->fd_file[0] = open(".tmp_heredoc", O_RDONLY);
 	}
-}
-
-void	here_command(char *hereline, char *herecom)
-{
-//	char		*herecom;
-
-//	herecom = NULL;
-	if (herecom)
-	{
-		printf("hereline is: %s\n", herecom);
-		herecom = ft_strjoin(herecom, hereline);
-		herecom = ft_strjoin(herecom, "\n");
-	}
-	else
-	{
-		printf("else\n");
-		herecom = ft_strjoin(hereline, "\n");
-		printf("herecom in the else: %s\n", herecom);
-
-	}
-//	return (herecom);
 }
