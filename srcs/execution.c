a00a364d6211e69fee33756a7a0354108a782347
KSura <kevin.sura@outlook.de>
2022-10-19 11:52:29 +0200

    dividing execution in multiple functions and files

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,70 +6,13 @@
 /*   By: ksura@student.42wolfsburg.de <ksura@studen +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:26:19 by ksura             #+#    #+#             */
-/*   Updated: 2022/10/18 17:27:50 by ksura@student.42 ###   ########.fr       */
+/*   Updated: 2022/10/19 11:50:34 by ksura@student.42 ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 #include <sys/types.h>
 #include <sys/wait.h>
-
-int	builtins(t_ms *ms)
-{
-	int	sum;
-
-	sum = (b_pwd(ms) + b_export(ms) + b_unset(ms) + b_echo(ms)
-			+ b_cd(ms) + b_exit(ms) + b_env(ms));
-	if (sum > 0)
-	{
-		printf("executed in builtins\n");
-		delete_section(ms);
-	}
-	return (sum);
-}
-
-void	child_signal(int sig)
-{
-	if (sig == SIGQUIT)
-		write(1, "Quit (core dumped)\n", 19);
-	if (sig == SIGINT)
-		write(1, "\b\b", 2);
-}
-
-void	child_infilefd(t_ms *ms, int in_pipe_fd)
-{
-	if (ms->pipes_struct->fd_file[0] >= 0)
-	{
-		close(STDIN_FILENO);
-		dup2(ms->pipes_struct->fd_file[0], STDIN_FILENO);
-		close(ms->pipes_struct->fd_file[0]);
-		ms->pipes_struct->fd_file[0] = -1;
-	}
-	else if (ms->pipes_struct->pipe_ends[0] >= 0 && ms->current_section != 0)
-	{
-		close(STDIN_FILENO);
-		dup2(in_pipe_fd, STDIN_FILENO);
-		close(in_pipe_fd);
-	}
-}
-
-void	child_outfilefd(t_ms *ms, int out_pipe_fd)
-{
-	if (ms->pipes_struct->fd_file[1] >= 0)
-	{
-		close(STDOUT_FILENO);
-		dup2(ms->pipes_struct->fd_file[1], STDOUT_FILENO);
-		close(ms->pipes_struct->fd_file[1]);
-		ms->pipes_struct->fd_file[1] = -1;
-	}
-	else if ((ms->pipes_struct->pipe_ends[1] >= 0
-			&& ms->current_section != ms->sections))
-	{
-		close(STDOUT_FILENO);
-		dup2(out_pipe_fd, STDOUT_FILENO);
-		close(out_pipe_fd);
-	}
-}
 
 void	childm(t_ms	*ms, int in_pipe_fd, int out_pipe_fd)
 {
@@ -85,6 +28,34 @@ void	childm(t_ms	*ms, int in_pipe_fd, int out_pipe_fd)
 	exit (127);
 }
 
+int	pipesandforks(int *out_pipe_fd, int *in_pipe_fd, t_ms *ms)
+{
+	if (ms->current_section % 2 == 0)
+	{
+		if (pipe(ms->pipes_struct->pipe_ends) == -1)
+			return (1);
+		if (ms->pipes_struct->pipe2_ends[0] != -1)
+			*in_pipe_fd = ms->pipes_struct->pipe2_ends[0];
+		*out_pipe_fd = ms->pipes_struct->pipe_ends[1];
+	}
+	else
+	{
+		if (pipe(ms->pipes_struct->pipe2_ends) == -1)
+			return (1);
+		*in_pipe_fd = ms->pipes_struct->pipe_ends[0];
+		*out_pipe_fd = ms->pipes_struct->pipe2_ends[1];
+	}
+	if (builtins(ms) == 0)
+	{
+		ms->pipes_struct->child_pid[0] = fork();
+		if (ms->pipes_struct->child_pid[0] == -1)
+			return (1);
+		if (ms->pipes_struct->child_pid[0] == 0)
+			childm(ms, *in_pipe_fd, *out_pipe_fd);
+	}
+	return (0);
+}
+
 int	multi_sections(t_ms	*ms)
 {
 	int	in_pipe_fd;
@@ -92,29 +63,8 @@ int	multi_sections(t_ms	*ms)
 
 	while (ms->current_section <= ms->sections)
 	{
-		if (ms->current_section % 2 == 0)
-		{
-			if (pipe(ms->pipes_struct->pipe_ends) == -1)
-				return (1);
-			if (ms->pipes_struct->pipe2_ends[0] != -1)
-				in_pipe_fd = ms->pipes_struct->pipe2_ends[0];
-			out_pipe_fd = ms->pipes_struct->pipe_ends[1];
-		}
-		else
-		{
-			if (pipe(ms->pipes_struct->pipe2_ends) == -1)
-				return (1);
-			in_pipe_fd = ms->pipes_struct->pipe_ends[0];
-			out_pipe_fd = ms->pipes_struct->pipe2_ends[1];
-		}
-		if (builtins(ms) == 0)
-		{
-			ms->pipes_struct->child_pid[0] = fork();
-			if (ms->pipes_struct->child_pid[0] == -1)
-				return (1);
-			if (ms->pipes_struct->child_pid[0] == 0)
-				childm(ms, in_pipe_fd, out_pipe_fd);
-		}
+		if (pipesandforks(&out_pipe_fd, &in_pipe_fd, ms) == 1)
+			return (1);
 		close(ms->pipes_struct->fd_file[0]);
 		ms->pipes_struct->fd_file[0] = -1;
 		close(ms->pipes_struct->fd_file[1]);
---
+++ srcs/execution2.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksura@student.42wolfsburg.de <ksura@studen +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 16:26:19 by ksura             #+#    #+#             */
+/*   Updated: 2022/10/19 11:50:42 by ksura@student.42 ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/minishell.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int	builtins(t_ms *ms)
+{
+	int	sum;
+
+	sum = (b_pwd(ms) + b_export(ms) + b_unset(ms) + b_echo(ms)
+			+ b_cd(ms) + b_exit(ms) + b_env(ms));
+	if (sum > 0)
+	{
+		printf("executed in builtins\n");
+		delete_section(ms);
+	}
+	return (sum);
+}
+
+void	child_signal(int sig)
+{
+	if (sig == SIGQUIT)
+		write(1, "Quit (core dumped)\n", 19);
+	if (sig == SIGINT)
+		write(1, "\b\b", 2);
+}
+
+void	child_infilefd(t_ms *ms, int in_pipe_fd)
+{
+	if (ms->pipes_struct->fd_file[0] >= 0)
+	{
+		close(STDIN_FILENO);
+		dup2(ms->pipes_struct->fd_file[0], STDIN_FILENO);
+		close(ms->pipes_struct->fd_file[0]);
+		ms->pipes_struct->fd_file[0] = -1;
+	}
+	else if (ms->pipes_struct->pipe_ends[0] >= 0 && ms->current_section != 0)
+	{
+		close(STDIN_FILENO);
+		dup2(in_pipe_fd, STDIN_FILENO);
+		close(in_pipe_fd);
+	}
+}
+
+void	child_outfilefd(t_ms *ms, int out_pipe_fd)
+{
+	if (ms->pipes_struct->fd_file[1] >= 0)
+	{
+		close(STDOUT_FILENO);
+		dup2(ms->pipes_struct->fd_file[1], STDOUT_FILENO);
+		close(ms->pipes_struct->fd_file[1]);
+		ms->pipes_struct->fd_file[1] = -1;
+	}
+	else if ((ms->pipes_struct->pipe_ends[1] >= 0
+			&& ms->current_section != ms->sections))
+	{
+		close(STDOUT_FILENO);
+		dup2(out_pipe_fd, STDOUT_FILENO);
+		close(out_pipe_fd);
+	}
+}
