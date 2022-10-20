b54179d9b105937c5e98eb112f035537fd00d4f6
olin_tp_ar <myemail@archi.com>
2022-10-20 13:54:47 +0200

    initializing sigaction struct, norminetted

--- srcs/parent_signals.c
+++ srcs/parent_signals.c
@@ -6,7 +6,7 @@
 /*   By: ksura@student.42wolfsburg.de <ksura@studen +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 13:31:26 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/10/19 12:07:48 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/10/20 13:54:05 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void	parent_signals(void)
 {
 	struct sigaction	sa;
 
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
 	sa.sa_handler = &handler_quit;
 	sigaction(SIGINT, &sa, NULL);
 	signal(SIGQUIT, SIG_IGN);
